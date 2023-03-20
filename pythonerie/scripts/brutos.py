78d8d71b386db65a6c68fdd6fa2aa9174ec4e159
Vehementz <vale-tudo91@sfr.fr>
2023-03-20 19:18:00 +0100

    line added to close the file

--- pythonerie/scripts/brutos.py
+++ pythonerie/scripts/brutos.py
@@ -6,37 +6,37 @@ import string
 import hashlib
 import sys
 
-### nombre aléatoire
-# print(random.randint(0, 244)) 
+# nombre aléatoire
+# print(random.randint(0, 244))
 
 
-
-#### Guess password
+# Guess password
 
 mot_de_passe = input("Quel est le mot de passe ? :")
 mot_de_passe_md5 = hashlib.md5(mot_de_passe.encode('utf8')).hexdigest()
 print(mot_de_passe_md5)
 
+
 def hash_crack():
     try:
-      mots_fr = open("/home/clement/Bureau/Cookbookerie/pythonerie/scripts/liste_francais.txt", "r")
-      for mot in mots_fr.readlines():
-          mot = mot.strip("\n").encode("utf8")
-          hashmd5 = hashlib.md5(mot).hexdigest()
-          if hashmd5 == mot_de_passe_md5:
-            print("Mot de passe trouvé : " + str(mot) + " (" + hashmd5 + ")")
-            trouve = True
-      if not trouve:
+        mots_fr = open(
+            "/home/clement/Bureau/Cookbookerie/pythonerie/scripts/liste_francais.txt", "r")
+        for mot in mots_fr.readlines():
+            mot = mot.strip("\n").encode("utf8")
+            hashmd5 = hashlib.md5(mot).hexdigest()
+            if hashmd5 == mot_de_passe_md5:
+                print("Mot de passe trouvé : " +
+                      str(mot) + " (" + hashmd5 + ")")
+                trouve = True
+        if not trouve:
             print("Mot de passe non trouvé :(")
+        mots_fr.close()
     except FileNotFoundError:
         print("Erreur : nom de dossier ou fichier introuvable !")
         sys.exit(1)
-
-    except Exception as err: 
-        print("Erreur : " + str(err))
+    except Exception as err:
         sys.exit(1)
         
-
 
 debut = time.time()
 hash_crack()
@@ -46,14 +46,17 @@ hash_crack()
 print("durée : " + str(time.time() - debut) + " secondes")
 # test
 def mot_aleatoire():
+
+
     lettres = string.ascii_letters
     suiv = ""
     resultat = ""
     for i in range(len(mot_de_passe)):
         while mot_de_passe[i] != suiv:
             suiv = random.choice(lettres)        
-        resultat += suiv        
-    return resultat
+            suiv = random.choice(lettres)
+        resultat += suiv
+
 
 debut = time.time()
 mot_aleatoire()
@@ -71,15 +74,12 @@ print("Durée: " + str(time.time() - debut) + " secondes")
 
 
 
-
-##### First program
-
+# First program
 # import random
 # import time
 
 # mot_de_passe = "mdpe" 
-
-
+# mot_de_passe = "mdpe"
 
 # def mot_aleatoire(longeur):
 #     lettres =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -100,6 +100,4 @@ print("Durée: " + str(time.time() - debut) + " secondes")
 #         fin = time.time() - debut
 #         print("Mot de passe trouvé ! en " + str(fin) + " secondes" + mot_alea)
 #         break
-
-
 
