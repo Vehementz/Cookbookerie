e5335596fd965f4f30816923e266ee37342631cb
Vehementz <vale-tudo91@sfr.fr>
2023-03-22 19:20:24 +0100

    first version made

--- pythonerie/project/onfile/rockpaperscissors.py
+++ pythonerie/project/onfile/rockpaperscissors.py
@@ -1,26 +1,33 @@
 import random
 
 options = ("rock", "paper", "scissors")
-player = None
-computer = random.choice(options)
+running = True
+while running:
+
+    player = None
+    computer = random.choice(options)
 
 
-while player not in options:
-    player = input("Enter a choice (rock, paper, scissors): ")
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors): ")
 
 
 
-print (f"Player: {player}")
-print(f"Computer: {computer}")
+    print (f"Player: {player}")
+    print(f"Computer: {computer}")
 
-if player == computer:
-    print("It's a tie!")
-elif player == "rock" and computer == "scissors":
-    print("You win")
-elif player == "papier" and computer == "rock":
-    print("You win")
-elif player == "scissors" and computer == "paper":
-    print("You win")
-else:
-    print("You lose!")
+    if player == computer:
+        print("It's a tie!")
+    elif player == "rock" and computer == "scissors":
+        print("You win")
+    elif player == "papier" and computer == "rock":
+        print("You win")
+    elif player == "scissors" and computer == "paper":
+        print("You win")
+    else:
+        print("You lose!")
 
+    if not input("Play again ? (y/n): ").lower() == "y":
+        running = False
+
+print("Thank for the game !")
