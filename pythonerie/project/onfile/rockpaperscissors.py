95d868cb99cd03f6984a0545dddcc8bffa3e5bd4
Vehementz <vale-tudo91@sfr.fr>
2023-03-22 19:23:46 +0100

    second version rock paper scissors done

--- pythonerie/project/onfile/rockpaperscissors.py
+++ pythonerie/project/onfile/rockpaperscissors.py
@@ -1,3 +1,5 @@
+### Second version
+
 import random
 
 options = ("rock", "paper", "scissors")
@@ -20,14 +22,54 @@ while running:
         print("It's a tie!")
     elif player == "rock" and computer == "scissors":
         print("You win")
-    elif player == "papier" and computer == "rock":
+    elif player == "paper" and computer == "rock":
         print("You win")
     elif player == "scissors" and computer == "paper":
         print("You win")
     else:
         print("You lose!")
 
-    if not input("Play again ? (y/n): ").lower() == "y":
+    play_again = input("Play again ? (y/n): ").lower()
+    if not play_again == "y":
         running = False
 
 print("Thank for the game !")
+
+
+
+
+##### First version 
+
+# import random
+
+# options = ("rock", "paper", "scissors")
+# running = True
+# while running:
+
+#     player = None
+#     computer = random.choice(options)
+
+
+#     while player not in options:
+#         player = input("Enter a choice (rock, paper, scissors): ")
+
+
+
+#     print (f"Player: {player}")
+#     print(f"Computer: {computer}")
+
+#     if player == computer:
+#         print("It's a tie!")
+#     elif player == "rock" and computer == "scissors":
+#         print("You win")
+#     elif player == "paper" and computer == "rock":
+#         print("You win")
+#     elif player == "scissors" and computer == "paper":
+#         print("You win")
+#     else:
+#         print("You lose!")
+
+#     if not input("Play again ? (y/n): ").lower() == "y":
+#         running = False
+
+# print("Thank for the game !")
