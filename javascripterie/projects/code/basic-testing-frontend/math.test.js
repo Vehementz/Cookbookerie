0969be7650408ad8f58b3e8a43f3acd38832b54f
Vehementz <vale-tudo91@sfr.fr>
2023-03-28 21:30:14 +0200

    2 others test added

--- javascripterie/projects/code/basic-testing-frontend/math.test.js
+++ javascripterie/projects/code/basic-testing-frontend/math.test.js
@@ -18,3 +18,23 @@ it('shold summarize all number values in an array', () => {
 });
 
 
+it('should yield NaN if a least one invalid nmber is provided', () => {
+    const inputs = ['invalid', 1];
+
+    const result = add(inputs);
+    expect(result).toBeNaN();
+});
+
+
+it('should yied a correct sum if an array of numeric string values is provided', () => {
+    const numbers = ['1', '2', '3'];
+    const result = add(numbers);
+
+    const expectedResult = numbers.reduce( 
+        (prevValue, curValue) => +prevValue + +curValue, 
+        0
+    );
+
+    expect(result).toBe(expectedResult);
+
+})
