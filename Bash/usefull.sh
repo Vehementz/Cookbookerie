744aac39615f59a94d0ac94da79645445a3fe6da
Vehementz <vale-tudo91@sfr.fr>
2023-03-29 12:30:53 +0200

    bash command usefull

---
+++ Bash/usefull.sh
@@ -0,0 +1,23 @@
+history 
+
+# show when pwd commmand have been used 
+history | grep pwd
+
+    # # # Return 
+    #   405  pwd
+    #   921  pwd
+    #   924  history | grep pwd
+    #   925  pwd
+    #   932  history | grep pwd
+
+#run the pwd with the 925 id that we se above
+!925
+
+
+# Show the sha256 checksum of the ye.txt file 
+sha256sum ye.txt
+
+
+
+## Create symbolic link
+ln -s /home/clement/Bureau/yu/ya/yu/ye ~/shortcut 
