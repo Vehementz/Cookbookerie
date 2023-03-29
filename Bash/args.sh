a1fe30ae1826cb46169b8cc6668ba3e073002d01
Vehementz <vale-tudo91@sfr.fr>
2023-03-29 21:00:25 +0200

    some exemples added in bash

---
+++ Bash/args.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for x in $@
+do
+    echo "Entered arg is $x"
+done
+
+
+# => ./args.sh yu ya
+
+# return
+# Entered arg is yu
+# Entered arg is ya
--- Bash/usefull.sh
+++ Bash/usefull.sh
@@ -24,3 +24,27 @@ ln -s /home/clement/Bureau/yu/ya/yu/ye ~/shortcut
 
 curl -C  -H "Authorization: Bearer ya29.beartruc" https://www.googleapis.com/drive/v3/files/truc -o namefile.file.vmdk
 
+
+
+# # # need to include the output of a complex command in your script, you can write the statement inside back ticks.
+#!/bin/bash
+
+var=`df -h | grep tmpfs`
+echo $var
+
+
+# Cron job example
+* * * * * sh /path/to/script.sh
+Here, * represent represents minute(s) hour(s) day(s) month(s) weekday(s), respectively.
+
+Below are some examples of scheduling cron jobs.
+SCHEDULE 	SCHEDULED VALUE
+5 0 * 8 * 	At 00:05 in August.
+5 4 * * 6 	At 04:05 on Sunday.
+0 22 * * 1-5 	At 22:00 on every day-of-week from Monday through Friday.
+
+
+
+crontab -l lists the already scheduled scripts for a particular user.
+
+find . -type f -name "*.sh"
