4e513e6ffdda4e2ff49bf88e3d65de7d8c13d665
Vehementz <vale-tudo91@sfr.fr>
2023-03-31 20:18:34 +0200

    two others test added for numbers.js file

--- javascripterie/projects/testing/basic-testing-frontend/src/util/numbers.test.js
+++ javascripterie/projects/testing/basic-testing-frontend/src/util/numbers.test.js
@@ -8,11 +8,31 @@ it('should transform a string number to a number of a type number', () => {
     expect(result).toBeTypeOf('number');
 });
 
+
+
+it('should transform a string number to a number of a type number', () => {
+    const input = '1';
+    const result = transformToNumber(input);
+    expect(result).toBe(+input);
+});
+
 it('should yield NaN for non-transformable values', () => {
     const input = 'invalid';
     const result = transformToNumber(input);
 
     expect(result).toBeNaN();
+});
 
-    
+
+it('should yield NaN for non-transformable values', () => {
+    const input = 'invalid';
+    const input2 = {};
+
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
+
+
+    expect(result).toBeNaN();
+    expect(result2).toBeNaN();
+
 });
