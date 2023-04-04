279487ccd4417d456c6373960b8259827d37f449
Vehementz <vale-tudo91@sfr.fr>
2023-04-04 13:04:33 +0200

    automated login done

---
+++ pythonerie/project/onfile/loginscrapper.py
@@ -0,0 +1,49 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+def get_drvier():
+  # Set options to make browsing easier
+  options = webdriver.ChromeOptions()
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized")
+  options.add_argument("disable-dev-shm-usage")
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches", ["enable-automation"])
+  options.add_argument("disable-blink-features=AutomationControlled")
+
+  driver = webdriver.Chrome(options=options)
+  driver.get("http://automated.pythonanywhere.com/login/")
+  return driver
+
+
+
+def clean_text(text):
+    """ Extract only the temperature from text"""
+    output = float(text.split(": ")[1])
+    return output
+
+def main():
+    driver = get_drvier()
+    driver.find_element(by="id", value="id_username").send_keys("automated")
+    time.sleep(2)
+    driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+    time.sleep(2)
+    driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
+    print(driver.current_url)
+    time.sleep(2)
+    temperaturetext = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]").text
+    return clean_text(temperaturetext)
+
+print(main())
+
+
+
+
+
+#### SOME DIFFERENTS THINGS CAN HELP 
+
+# pip3 install webdriver-manager
+# sudo apt install chromium-chromedriver
+# pip install -U selenium
